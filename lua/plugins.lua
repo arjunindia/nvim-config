7f6718237dc693f9ae1e20fec5ecb353e5d182cc
Arjun <arjun8107@gmail.com>
2024-03-29 22:53:07 +0530

    format on save fixes

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,31 +1,32 @@
-
 return {
-{
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
-},
-{
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-},
-{ "tiagovla/tokyodark.nvim" }, -- lazy
-{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-'mbbill/undotree',
---- Uncomment the two plugins below if you want to manage the language servers from neovim
-{'williamboman/mason.nvim'},
-{'williamboman/mason-lspconfig.nvim'},
-{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-{'neovim/nvim-lspconfig'},
-{'hrsh7th/cmp-nvim-lsp'},
-{'hrsh7th/nvim-cmp'},
-{'L3MON4D3/LuaSnip'},
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.6',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { "tiagovla/tokyodark.nvim" }, -- lazy
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    'mbbill/undotree',
+    --- Uncomment the two plugins below if you want to manage the language servers from neovim
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+    { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'L3MON4D3/LuaSnip' },
+    { "elentok/format-on-save.nvim" },
 }
