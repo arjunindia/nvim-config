049e0f627c511f55f2b7d4faf62511715035ee1e
Arjun <arjun8107@gmail.com>
2024-03-29 21:57:37 +0530

    lsp

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,51 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+-- to learn how to use mason.nvim with lsp-zero
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {},
+  handlers = {
+    lsp_zero.default_setup,
+  },
+})
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+  },
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+    ['<C-e>'] = cmp.mapping.abort();
+    ['<C-Tab>'] = cmp.mapping.complete(),
+    ['<Up>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+    ['<Down>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+    ['<C-p>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item({behavior = 'insert'})
+      else
+        cmp.complete()
+      end
+    end),
+    ['<C-n>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_next_item({behavior = 'insert'})
+      else
+        cmp.complete()
+      end
+    end),
+  },
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,4 +20,12 @@ return {
 { "tiagovla/tokyodark.nvim" }, -- lazy
 {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
 'mbbill/undotree',
+--- Uncomment the two plugins below if you want to manage the language servers from neovim
+{'williamboman/mason.nvim'},
+{'williamboman/mason-lspconfig.nvim'},
+{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+{'neovim/nvim-lspconfig'},
+{'hrsh7th/cmp-nvim-lsp'},
+{'hrsh7th/nvim-cmp'},
+{'L3MON4D3/LuaSnip'},
 }
